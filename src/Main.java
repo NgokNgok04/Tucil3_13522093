4daec667754353f2bbd6eb01416ef9af1c4390b8
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-05-07 08:37:46 +0700

    cleaning

--- src/Main.java
+++ src/Main.java
@@ -39,106 +39,41 @@ public class Main {
         MyDictionary dictionary = new MyDictionary();
         int len = 0;
         int nodeVisited = 0;
-        if(methodInput.equals("1")){
-            List<String> wordListQueue = new ArrayList<>();
-            solution = greedyObject.algorithmGreedy(startInput,endInput,wordListQueue,visitedWord, dictionary);
-            len = solution.size() - 1;
-            nodeVisited = solution.size();
-        } else if (methodInput.equals("2")){
-            solution = ucsObject.algorithmUCS(startInput, endInput, wordQueue, visitedWord, dictionary,0);
-            len = solution.size() - 2;
-            nodeVisited = Integer.parseInt(solution.get(solution.size() - 1));
+        if (startInput.length() == endInput.length() && dictionary.isAValidWord(startInput) && dictionary.isAValidWord(endInput)){
 
-        } else {
-            System.out.println("Masuk ke A*");
-            solution = aStarObject.algorithmAstar(startInput, endInput, wordQueue, visitedWord, dictionary,0);
-            len = solution.size() - 2;
-            nodeVisited = Integer.parseInt(solution.get(solution.size() - 1));
-        }
-        Instant endTime = Instant.now();
-
-        Duration duration = Duration.between(startTime, endTime);
-        System.out.println("\n\nProgram execution duration: " + duration.toMillis() + " milliseconds");
-        System.out.println("Solution length : " + len);
-        System.out.println("Node Visited: " + nodeVisited);
-        System.out.print("[");
-        for(int i = 0; i < solution.size() - 1; i++){
-            System.out.print(solution.get(i));
-            if (i != solution.size() - 2){
-                System.out.print(",");
+            if(methodInput.equals("1")){
+                List<String> wordListQueue = new ArrayList<>();
+                solution = greedyObject.algorithmGreedy(startInput,endInput,wordListQueue,visitedWord, dictionary);
+                len = solution.size() - 1;
+                nodeVisited = solution.size();
+            } else if (methodInput.equals("2")){
+                solution = ucsObject.algorithmUCS(startInput, endInput, wordQueue, visitedWord, dictionary,0);
+                len = solution.size() - 2;
+                nodeVisited = Integer.parseInt(solution.get(solution.size() - 1));
+                
+            } else {
+                System.out.println("Masuk ke A*");
+                solution = aStarObject.algorithmAstar(startInput, endInput, wordQueue, visitedWord, dictionary,0);
+                len = solution.size() - 2;
+                nodeVisited = Integer.parseInt(solution.get(solution.size() - 1));
             }
+            Instant endTime = Instant.now();
+            
+            Duration duration = Duration.between(startTime, endTime);
+            System.out.println("\n\nProgram execution duration: " + duration.toMillis() + " milliseconds");
+            System.out.println("Solution length : " + len);
+            System.out.println("Node Visited: " + nodeVisited);
+            System.out.print("[");
+            for(int i = 0; i < solution.size() - 1; i++){
+                System.out.print(solution.get(i));
+                if (i != solution.size() - 2){
+                    System.out.print(",");
+                }
+            }
+            System.out.print("]");
+        } else {
+            System.out.println("Masukan tidak Valid");
         }
-        System.out.print("]");
         scanner.close();
     }
 }
-
-
-
-
-
-    // private static Instant startTime;
-    // public static void main(String[] args) throws IOException {
-    //     HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-    //     server.createContext("/", new MyHandler());
-    //     server.setExecutor(null); // creates a default executor
-    //     server.start();
-    //     System.out.println("Server started on port 8000");
-    // }
-
-    // static class MyHandler implements HttpHandler {
-    //     @Override
-    //     public void handle(HttpExchange exchange) throws IOException {
-    //         if ("POST".equals(exchange.getRequestMethod())) {
-    //             // Read the request body
-    //             InputStream requestBody = exchange.getRequestBody();
-    //             InputStreamReader requestBodyReader = new InputStreamReader(requestBody);
-    //             StringBuilder requestBodyBuilder = new StringBuilder();
-    //             int c;
-    //             while ((c = requestBodyReader.read()) != -1) {
-    //                 requestBodyBuilder.append((char) c);
-    //             }
-    //             requestBodyReader.close();
-
-    //             String requestBodyString = requestBodyBuilder.toString();
-    //             Map<String, String> requestData = parseJson(requestBodyString);
-
-    //             String startWord = requestData.get("startWord");
-    //             String endWord = requestData.get("endWord");
-    //             String algorithmType = requestData.get("algorithmType");                
-
-    //             Process processObject = new Process(startWord, endWord, algorithmType);
-    //             String responseData = String.format("{\"timeExecution\": %d, \"nodeVisited\": %d, \"len\": %d, \"solution\": %s}", processObject.timeExecution, processObject.nodeVisited, processObject.len, Arrays.toString(processObject.solution));
-
-    //             Headers responseHeaders = exchange.getResponseHeaders();
-    //             responseHeaders.set("Content-Type", "application/json");
-    //             responseHeaders.set("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
-    //             responseHeaders.set("Access-Control-Allow-Methods", "POST"); // Allow only POST requests
-    //             responseHeaders.set("Access-Control-Allow-Headers", "Content-Type"); // Allow only specific headers
-
-    //             exchange.sendResponseHeaders(200, responseData.getBytes().length);
-    //             OutputStream os = exchange.getResponseBody();
-    //             os.write(responseData.getBytes());
-    //             os.close();
-    //         } else {
-    //             // Method not allowed
-    //             exchange.sendResponseHeaders(405, -1);
-    //         }
-    //     }
-    
-    //     private Map<String, String> parseJson(String json) {
-    //         Map<String, String> data = new HashMap<>();
-    //         // Simple JSON parsing - You can replace this with your own parsing logic
-    //         String[] parts = json.split(",");
-    //         for (String part : parts) {
-    //             String[] keyValue = part.split(":");
-    //             if (keyValue.length == 2) {
-    //                 String key = keyValue[0].trim().replaceAll("\"", "");
-    //                 String value = keyValue[1].trim().replaceAll("\"", "");
-    //                 data.put(key, value);
-    //             }
-    //         }
-    //         return data;
-    //     }
-    // }
-
